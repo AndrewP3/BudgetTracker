9d75d71a71df52280dd71436a90e37b69faf4cd7
AndrewP3 <andrewpowell097@gmail.com>
2021-08-31 14:40:43 -0700

    3rd commit

--- Develop/public/serviceWorker.js
+++ Develop/public/serviceWorker.js
@@ -11,3 +11,58 @@ self.addEventListener("Install", (event) => {
   );
   self.skipWaiting();
 });
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (
+    event.request.method !== "GET" ||
+    !event.request.url.startsWith(self.location.origin)
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  if (event.request.url.includes("/api/transaction")) {
+    event.respondWith(
+      caches.open(RUNTIME_CACHE).then((cache) =>
+        fetch(event.request)
+          .then((response) => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => caches.match(event.request))
+      )
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return caches
+        .open(RUNTIME_CACHE)
+        .then((cache) =>
+          fetch(event.request).then((response) =>
+            cache.put(event.request, response.clone()).then(() => response)
+          )
+        );
+    })
+  );
+});
